c46e03c4f53e44b1f2c2c9eeb251c5c46d910023
Abdalrahman-22 <www.abdalrahman95.3@gmail.com>
2023-09-16 13:25:50 +0300

    delete deprecated

--- my-library/my_library.cpp
+++
@@ -1,6 +0,0 @@
-#include "my_library.h"
-
-int addition(int a, int b)
-{
-  return a+b;
-  }
--- my-library/my_library.h
+++
@@ -1,8 +0,0 @@
-#ifndef my_library_h
-#define my_library_h
-
-#include <Arduino.h>
-
-int addition(int a,int b);
-
-#endif
