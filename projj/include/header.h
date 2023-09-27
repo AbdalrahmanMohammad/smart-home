ee4be614653326732c1d960af19a150f14e5397f
Abdalrahman-22 <www.abdalrahman95.3@gmail.com>
2023-09-27 19:58:24 +0300

    return to my wifi & solve some library issue & use bootstrap

--- projj/include/header.h
+++ projj/include/header.h
@@ -1,12 +1,12 @@
 #include "../lib/LedClass.h" // include the classes you make here
 
 // Set your network credentials
-const char* ssid = "PLL";
+const char* ssid = "PL";
 const char* password = "87654321";
 
 // Set your static IP address here
-IPAddress staticIP(192, 168, 154, 177); 
-IPAddress gateway(192, 168, 154, 1);    
+IPAddress staticIP(192, 168, 8, 177); 
+IPAddress gateway(192, 168, 8, 1);    
 IPAddress subnet(255, 255, 255, 0);  
 
 AsyncWebServer server(80);
