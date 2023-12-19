5412eafb2593066def4b695e269750191429130c
Abdalrahman-22 <www.abdalrahman95.3@gmail.com>
2023-12-19 20:44:36 +0200

    added backend codes

---
+++ back/getdata.php
@@ -0,0 +1,31 @@
+<?php
+include 'database.php';
+
+if (!empty($_POST)) {
+
+    $id = $_POST['id'];
+
+    $myObj = (object) array();   
+
+    //........................................ 
+    $pdo = Database::connect();
+
+    $sql = 'SELECT * FROM led WHERE id="' . $id . '"';
+    foreach ($pdo->query($sql) as $row) {
+        $date = date_create($row['date']);
+        $dateFormat = date_format($date, "d-m-Y");
+
+        $myObj->id = $row['id'];
+        $myObj->LED = $row['LED'];
+        $myObj->ls_time = $row['time'];
+        $myObj->ls_date = $dateFormat;
+
+        $myJSON = json_encode($myObj);
+
+        echo $myJSON;
+    }
+    Database::disconnect();
+    //........................................ 
+}
+//---------------------------------------- 
+?>
---
+++ back/updatedata.php
@@ -0,0 +1,39 @@
+<?php
+require 'database.php';
+
+//---------------------------------------- Condition to check that POST value is not empty.
+if (!empty($_POST)) {
+    //........................................ keep track POST values
+    $id = $_POST['id'];
+    $led_state = $_POST['led'];
+    //........................................
+
+    //........................................ Get the time and date.
+    date_default_timezone_set("Asia/Jerusalem");
+    $tm = date("H:i:s");
+    $dt = date("Y-m-d");
+    //........................................
+
+    //........................................ Updating the data in the table.
+    $pdo = Database::connect();
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = 'SELECT * FROM led WHERE id="' . $id . '"';
+    $q = $pdo->prepare($sql);
+    $q->execute();
+
+    $state = "unknown";
+    if ($row = $q->fetch(PDO::FETCH_ASSOC)) {
+        $state = $row['LED'];
+    }
+
+    if ($state != $led_state && $state != "unknown") {
+        $sql = "UPDATE led SET LED = ?, time = ?, date = ? WHERE id = ?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($led_state, $tm, $dt, $id));
+    }
+
+    Database::disconnect();
+    //........................................
+}
+?>
